5d5c7bdd77a20ac736af0366fd3df7f1ad360e09
N. Dimer <ndimer@cloudbeat.io>
2020-02-22 13:33:31 +0200

    Improve step label for various web driver actions.

--- src/main/java/io/cloudbeat/common/CbEventHandler.java
+++ src/main/java/io/cloudbeat/common/CbEventHandler.java
@@ -8,148 +8,204 @@ import org.openqa.selenium.support.events.WebDriverEventListener;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Hashtable;
 
 public class CbEventHandler implements WebDriverEventListener {
 
     private CbTest currentTest;
+    private final Hashtable<Integer, String> elementHash = new Hashtable();
 
-    public CbEventHandler(CbTest test) {
+    public CbEventHandler(CbTest test)
+    {
         currentTest = test;
     }
 
     @Override
-    public void beforeAlertAccept(WebDriver webDriver) {
+    public void beforeAlertAccept(final WebDriver webDriver) {
         currentTest.startStep("Alert accepting");
     }
 
     @Override
-    public void afterAlertAccept(WebDriver webDriver) {
+    public void afterAlertAccept(final WebDriver webDriver) {
         currentTest.endStep("Alert accepting");
     }
 
     @Override
-    public void afterAlertDismiss(WebDriver webDriver) {
+    public void afterAlertDismiss(final WebDriver webDriver) {
         currentTest.startStep("Alert dismissing");
     }
 
     @Override
-    public void beforeAlertDismiss(WebDriver webDriver) {
+    public void beforeAlertDismiss(final WebDriver webDriver) {
         currentTest.endStep("Alert dismissing");
     }
 
     @Override
-    public void beforeNavigateTo(String s, WebDriver webDriver) {
+    public void beforeNavigateTo(final String s, final WebDriver webDriver) {
         currentTest.startStep("Navigate to " + s);
     }
 
     @Override
-    public void afterNavigateTo(String s, WebDriver webDriver) {
+    public void afterNavigateTo(final String s, final WebDriver webDriver) {
         currentTest.endStep("Navigate to " + s);
     }
 
     @Override
-    public void beforeNavigateBack(WebDriver webDriver) {
+    public void beforeNavigateBack(final WebDriver webDriver) {
         currentTest.startStep("Navigate back");
     }
 
     @Override
-    public void afterNavigateBack(WebDriver webDriver) {
+    public void afterNavigateBack(final WebDriver webDriver) {
         currentTest.endStep("Navigate back");
     }
 
     @Override
-    public void beforeNavigateForward(WebDriver webDriver) {
+    public void beforeNavigateForward(final WebDriver webDriver) {
         currentTest.startStep("Navigate forward");
     }
 
     @Override
-    public void afterNavigateForward(WebDriver webDriver) {
+    public void afterNavigateForward(final WebDriver webDriver) {
         currentTest.endStep("Navigate forward");
     }
 
     @Override
-    public void beforeNavigateRefresh(WebDriver webDriver) {
+    public void beforeNavigateRefresh(final WebDriver webDriver) {
         currentTest.startStep("Navigate refresh");
     }
 
     @Override
-    public void afterNavigateRefresh(WebDriver webDriver) {
+    public void afterNavigateRefresh(final WebDriver webDriver) {
         currentTest.endStep("Navigate refresh");
     }
 
     @Override
-    public void beforeFindBy(By by, WebElement webElement, WebDriver webDriver) {
-        currentTest.startStep("Finding element " + by.toString());
+    public void beforeFindBy(final By by, final WebElement webElement, final WebDriver webDriver) {
+        final String locatorDispName = this.getLocatorDisplayName(by);
+        currentTest.startStep(String.format("Find element %s",  locatorDispName));
     }
 
     @Override
-    public void afterFindBy(By by, WebElement webElement, WebDriver webDriver) {
-        currentTest.endStep("Finding element " + by.toString());
+    public void afterFindBy(final By by, final WebElement webElement, final WebDriver webDriver) {
+        elementHash.put(webElement.hashCode(), by.toString());
+        final String locatorDispName = this.getLocatorDisplayName(by);
+        currentTest.endStep(String.format("Find element %s",  locatorDispName));
     }
 
     @Override
-    public void beforeClickOn(WebElement webElement, WebDriver webDriver) {
-        currentTest.startStep("Click on " + webElement.getText());
+    public void beforeClickOn(final WebElement webElement, final WebDriver webDriver) {
+        final String elmName = this.getElementDisplayName(webElement);
+        currentTest.startStep(String.format("Click on %s", elmName));
     }
 
     @Override
-    public void afterClickOn(WebElement webElement, WebDriver webDriver) {
-        currentTest.endStep("Click on " + webElement.getText());
+    public void afterClickOn(final WebElement webElement, final WebDriver webDriver) {
+        final String elmName = this.getElementDisplayName(webElement);
+        currentTest.endStep(String.format("Click on %s", elmName));
     }
 
     @Override
-    public void beforeChangeValueOf(WebElement webElement, WebDriver webDriver, CharSequence[] charSequences) {
-        currentTest.startStep("Change value of " + webElement.getText());
+    public void beforeChangeValueOf(final WebElement webElement, final WebDriver webDriver,
+            final CharSequence[] charSequences) {
+        final StringBuilder sb = new StringBuilder();
+        for (final CharSequence charSequence : charSequences) {
+            sb.append(charSequence.toString());
+        }
+        currentTest.startStep(String.format("Set value \"%s\"", sb.toString()));
     }
 
     @Override
-    public void afterChangeValueOf(WebElement webElement, WebDriver webDriver, CharSequence[] charSequences) {
-        currentTest.endStep("Change value of " + webElement.getText());
+    public void afterChangeValueOf(final WebElement webElement, final WebDriver webDriver,
+            final CharSequence[] charSequences) {
+        final StringBuilder sb = new StringBuilder();
+        for (final CharSequence charSequence : charSequences) {
+            sb.append(charSequence.toString());
+        }
+        currentTest.endStep(String.format("Set value \"%s\"", sb.toString()));
     }
 
     @Override
-    public void beforeScript(String s, WebDriver webDriver) {
+    public void beforeScript(final String s, final WebDriver webDriver) {
         currentTest.startStep("Executing script " + s);
     }
 
     @Override
-    public void afterScript(String s, WebDriver webDriver) {
+    public void afterScript(final String s, final WebDriver webDriver) {
         currentTest.endStep("Executing script " + s);
     }
 
     @Override
-    public void beforeSwitchToWindow(String s, WebDriver webDriver) {
+    public void beforeSwitchToWindow(final String s, final WebDriver webDriver) {
         currentTest.startStep("Switch to window " + s);
     }
 
     @Override
-    public void afterSwitchToWindow(String s, WebDriver webDriver) {
+    public void afterSwitchToWindow(final String s, final WebDriver webDriver) {
         currentTest.endStep("Switch to window " + s);
     }
 
     @Override
-    public void onException(Throwable throwable, WebDriver webDriver) {
-        FailureModel failureModel = new FailureModel(throwable);
+    public void onException(final Throwable throwable, final WebDriver webDriver) {
+        final FailureModel failureModel = new FailureModel(throwable);
         currentTest.failCurrentStep(failureModel);
     }
 
     @Override
-    public <X> void beforeGetScreenshotAs(OutputType<X> outputType) {
+    public <X> void beforeGetScreenshotAs(final OutputType<X> outputType) {
 
     }
 
     @Override
-    public <X> void afterGetScreenshotAs(OutputType<X> outputType, X x) {
+    public <X> void afterGetScreenshotAs(final OutputType<X> outputType, final X x) {
 
     }
 
     @Override
-    public void beforeGetText(WebElement webElement, WebDriver webDriver) {
+    public void beforeGetText(final WebElement webElement, final WebDriver webDriver) {
         currentTest.startStep("Getting text of  " + webElement.getText());
     }
 
     @Override
-    public void afterGetText(WebElement webElement, WebDriver webDriver, String s) {
+    public void afterGetText(final WebElement webElement, final WebDriver webDriver, final String s) {
         currentTest.endStep("Getting text of  " + webElement.getText());
+    }
+
+    private String getLocatorDisplayName(final By by) {
+        final String byLocatorStr = by.toString();
+        return String.format("%s", byLocatorStr.replace("By.", "by "));
+    }
+
+    private String getElementDisplayName(final WebElement webElement) {
+        final String text = webElement.getText();
+        final String tagName = webElement.getTagName();
+        final String elmType = webElement.getAttribute("type");
+        String elmTypeLabel = "";
+        // determine element type (link, button or other)
+        if (tagName.equals("a")) {
+            elmTypeLabel = "link ";
+        }            
+        else if (tagName.equals("button")) {
+            elmTypeLabel = "button ";
+        }
+        else if (tagName.equals("option")) {
+            elmTypeLabel = "option ";
+        }
+        else if (tagName.equals("label")) {
+            elmTypeLabel = "label ";
+        }
+        else if (tagName.equals("input") && elmType != null && (elmType.equals("button") || elmType.equals("submit"))) {
+            elmTypeLabel = "button ";
+        }
+        else if (tagName.equals("input") && elmType != null && elmType.equals("url")) {
+            elmTypeLabel = "link ";
+        }
+
+        if (text != null && !text.isEmpty()) {
+            return String.format("%s\"%s\"", elmTypeLabel, text);
+        }
+        else {
+            return String.format("<%s>", tagName);
+        }
     }
 }
